56c0968b64225b107fc507e6772b0617c37dad51
Edwin Cardenas <eacardenase@gmail.com>
2024-12-27 18:07:07 -0500

    feat: add remainingAngle func implementation

--- Triangle/main.c
+++ Triangle/main.c
@@ -7,8 +7,19 @@
 
 #include <stdio.h>
 
+static float triangleDegreeLimit = 180.0;
+
+float remainingAngle(float angleA, float angleB)
+{
+    return triangleDegreeLimit - (angleA + angleB);
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    float angleA = 30.0;
+    float angleB = 60.0;
+    float angleC = remainingAngle(angleA, angleB);
+    
+    printf("The third angle is %.2f\n", angleC);
+    
     return 0;
 }
